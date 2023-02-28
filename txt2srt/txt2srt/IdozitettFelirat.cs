be345f8da6d9059221bde4032869f54d181cb1ae
Konyicska-Kristof <113448136+Konyicska-Kristof@users.noreply.github.com>
2023-02-28 09:37:40 +0100

    Add files via upload

---
+++ txt2srt/txt2srt/IdozitettFelirat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace txt2srt
+{
+    internal class IdozitettFelirat
+    {
+        public string Idozites;
+        public string Felirat;
+        public IdozitettFelirat(string Sor)
+        {
+            int i = 0;
+            bool not_int = false;
+            while (i<10 || not_int!=true)
+            {
+                if (Sor.First() != char.Parse(i.ToString()))
+                {
+                    not_int = true;
+                }
+                i++;
+                
+            }
+
+            if (not_int==true)
+            {
+                Felirat = Sor;
+            }
+            else
+            {
+                 Idozites = Sor;
+            }
+            
+
+        }
+
+    }
+}
---
+++ txt2srt/txt2srt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace txt2srt
+{    
+    internal class Program
+    {                       
+        static void Main(string[] args)
+        {
+            List<string> L_Idoz = new();
+            List<string> L_Fel = new();
+
+            using StreamReader sr = new(@"..\..\..\src\feliratok.txt", Encoding.UTF8);
+
+            while (!sr.EndOfStream)
+            {
+                L_Idoz.Add(new IdozitettFelirat(sr.ReadLine()).Idozites);
+                L_Fel.Add(new IdozitettFelirat(sr.ReadLine()).Felirat);
+            }
+
+            Console.WriteLine($"5. Feladat - Feliratok száma: {L_Fel.Count()}");
+        }
+    }
+}
